0c4bedcd5f2a629d66c0bb6540a1e29c8b7f4dcf
sigoden <sigoden@gmail.com>
2018-01-16 22:26:28 +0800

    修复打包生成的 crx 崩溃问题

--- utils/dist.js
+++ utils/dist.js
@@ -6,10 +6,8 @@ const fs = require('fs')
 const PEM_FILE = process.env.PEM_FILE || './build.pem'
 const path = require('path')
 const appDir = path.resolve(__dirname, '..')
-const pkg = require(path.join(appDir, 'package.json'))
 const buildDir = path.join(appDir, 'build')
 const distDir = path.join(appDir, 'dist')
-const outputFile = path.join(distDir, `chrome-extensions-manager_v${pkg.version}`)
 const cp = require('child_process')
 
 if (!fs.existsSync(PEM_FILE)) {
@@ -17,13 +15,25 @@ if (!fs.existsSync(PEM_FILE)) {
   process.exit(1)
 }
 
-const cmd = `npx crx pack ${buildDir} -o ${outputFile}.crx --zip-output ${outputFile}.zip -p ${PEM_FILE}`
+let manifestPath = path.join(buildDir, 'manifest.json')
+let manifestContent = fs.readFileSync(manifestPath, {encoding: 'utf8'})
+let manifest = JSON.parse(manifestContent)
+let outputFile = path.join(distDir, `chrome-extensions-manager_v${manifest.version}`)
+
+let cmd = `npx crx pack ${buildDir} -o ${outputFile}.crx --zip-output ${outputFile}.zip -p ${PEM_FILE}`
 cp.exec(cmd, function (err, stdout, stderr) {
   if (err) throw err
   if (stderr) {
     console.error(stdout)
   }
-  if (stdout) {
-    console.log(stdout)
-  }
+  manifest.update_url = 'http://localhost:3000/update2/crx'
+  fs.writeFileSync(manifestPath, JSON.stringify(manifest))
+  cmd = `npx crx pack ${buildDir} -o ${outputFile}.crx -p ${PEM_FILE}`
+  cp.exec(cmd, function (err, stdout, stderr) {
+    fs.writeFileSync(manifestPath, manifestContent)
+    if (err) throw err
+    if (stderr) {
+      console.error(stdout)
+    }
+  })
 })
